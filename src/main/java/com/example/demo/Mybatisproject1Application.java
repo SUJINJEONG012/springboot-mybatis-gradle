cf41d5a68c9eb5dc60d8ae4fb0f2586128a07c23
angela <peekaboo32@naver.com>
2023-06-29 14:35:16 +0900

    스프링부트로 마이바티스 연동하기

---
+++ src/main/java/com/example/demo/Mybatisproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatisproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mybatisproject1Application.class, args);
+	}
+
+}
